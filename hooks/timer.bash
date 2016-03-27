d64e46dce233a33cba00b61b21da727e20d21733
Joe Fifield <jfifield@programmerplanet.org>
2016-03-27 11:00:12 -0400

    Fix bug in timer hook on ubuntu

--- hooks/timer.bash
+++ hooks/timer.bash
@@ -1,8 +1,6 @@
 function _sbp_timer_start { # Timer of last timer
   if [[ -z "$_sbp_timer_complete" && -z "$_sbp_timer" ]]; then
     _sbp_timer=$SECONDS
-  else
-    unset _sbp_timer_complete
   fi
 }
 
@@ -10,12 +8,12 @@ function _sbp_timer_stop {
   local seconds=$(( SECONDS - _sbp_timer ))
   _sbp_timer_m=$(( seconds / 60 ))
   _sbp_timer_s=$(( seconds - (60 * _sbp_timer_m) ))
-  _sbp_timer_complete=1
 }
 
 function _sbp_trigger_timer_hook {
   _sbp_timer_stop
   unset _sbp_timer
+  unset _sbp_timer_complete
 }
 
 trap '_sbp_timer_start' DEBUG
